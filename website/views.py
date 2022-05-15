6fb7242d73da6dc08f363577c32182f7f1963e79
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-15 18:56:27 +0300

    fix:fixed a bug in views.py

--- website/views.py
+++ website/views.py
@@ -25,6 +25,11 @@ def home():
     posts = Post.query.all()
     return render_template("home.html", user=current_user, posts=posts,data=data)
 
+######################
+## func=            ##
+## delete a post    ##
+######################
+
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
@@ -65,27 +70,12 @@ def create_post():
 
     return render_template('create_post.html', user=current_user)
 
+
 ######################
 ## func=            ##
-## delete a post    ##
+## display a post   ##
 ######################
 
-
-# @views.route("/delete-post/<id>")
-# @login_required
-# def delete_post(id):
-#     post = Post.query.filter_by(id=id).first()
-
-#     if not post:
-#         flash("Post does not exist.", category='error')
-#     elif current_user.id != post.id:
-#         flash('You do not have permission to delete this post.', category='error')
-#     else:
-#         db.session.delete(post)
-#         db.session.commit()
-#         flash('Post deleted.', category='success')
-
-#     return redirect(url_for('views.home'))
 
 @views.route("/posts/<username>")
 @login_required
@@ -142,25 +132,6 @@ def delete_comment(comment_id):
         db.session.commit()
 
     return redirect(url_for('views.home'))
-# @views.route("/like-post/<post_id>", methods=['POST'])
-# @login_required
-# def like(post_id):
-#     post = Post.query.filter_by(id=post_id).first()
-#     like = Like.query.filter_by(
-#         author=current_user.id, post_id=post_id).first()
 
-#     if not post:
-#         return jsonify({'error': 'Post does not exist.'}, 400)
-#     elif like:
-#         db.session.delete(like)
-#         db.session.commit()
-#     else:
-#         like = Like(author=current_user.id, post_id=post_id)
-#         db.session.add(like)
-#         db.session.commit()
-
-#     return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
-
-    
     
 
